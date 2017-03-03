e19b528b23d1f720ec01934cdd3d715709606eed
Douglas da Silva <douglas@neoprospecta.com>
2017-03-03 01:10:50 -0300

    att templates / forms

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,11 @@
-from .models import Candidato
+from .models import Candidato, Criterio
 from django import forms
 
 class CandForm(forms.ModelForm):
 	class Meta:
 		model = Candidato
 		fields = ('name', 'e_mail', 'github', 'linkedin', 'Ensino_superior')
+
+
+
+		
--- app/urls.py
+++ app/urls.py
@@ -4,10 +4,8 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.canditato_list, name='canditato_list'),
-	#url(r'^avaliacao/$', views.avaliacao, name='avaliacao')
 	url(r'^candidato/(?P<pk>[0-9]+)/$', views.candidato_detalhe, name='candidato_detalhe'),
-	#url(r'^candidato/new/$', views.cadastrar, name='cadastrar'), 	<-- o botao fica na msm page que lista cand
-	url(r'^candidato/[0-9]+/new/$', views.cadastrar, name='cadastrar'),				# <-- o botao fica num link independete
-	#url(r'^avaliacao/$', views.avaliar, name='avaliar'), 
+	url(r'^cadastrar/$', views.cadastrar, name='cadastrar'),	# <--! o cadastro fica num link independete
+	url(r'^candidato/[0-9]+/avaliacao/$', views.avaliar, name='avaliar'), 
 
 ]
