4f17fb3e15ad31472328db97c2f566c634b1ae7a
Douglas da Silva <douglas@neoprospecta.com>
2017-03-28 04:17:27 -0300

    migração para postgresql

--- site_/settings.py
+++ site_/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,28 +75,31 @@ WSGI_APPLICATION = 'site_.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 """
+DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+    'default': dj_database_url.config(default='postgres://localhost:5432/postgres_db_name'),
+}
+"""
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
+        'NAME': 'douglas_db',
+        'USER': 'douglas',
+        'PASSWORD': 'vamointer',
         'HOST': '127.0.0.1',
-        'PORT': '   ',
+        'PORT': '5432',
     }
 }
-"""
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
+
+#DATABASES = {'default': dj_database_url.config(default='postgres://127.0.0.1:5432/postgres_db_name')}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
